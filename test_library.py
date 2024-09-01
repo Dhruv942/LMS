e173c82800c3a1d23a11ffb569310511e123751c
Dhruv Patel <dhruvpatel242231@gmail.com>
2024-09-01 12:24:27 +0530

    Refactore testcases for add multiplebook

--- test_library.py
+++ test_library.py
@@ -7,6 +7,15 @@ class TestLibrary(unittest.TestCase):
         book = Book(isbn='1234567890', title='Test-Driven Development', author='xyz', year=2002)
         library.add_book(book)
         self.assertEqual(library.books[0].isbn, '1234567890')
+    def test_add_multiplebook(self):
+        library = Library()
+        book1 = Book(isbn='1234567890', title='Test-Driven Development', author='xyz', year=2002)
+        book2 = Book(isbn='123456789', title='python', author='abc', year=2002)
+        library.add_book(book1)
+        library.add_book(book2)
+        self.assertEqual(library.books[0].isbn, '1234567890')
+        self.assertEqual(library.books[1].isbn, '123456789')
+    
 
 if __name__ == '__main__':
     unittest.main()
