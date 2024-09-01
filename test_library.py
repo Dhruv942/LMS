2c9939ca9563b2e10050632860b777228d3803a1
Dhruv Patel <dhruvpatel242231@gmail.com>
2024-09-01 18:30:13 +0530

    Refactor code

--- test_library.py
+++ test_library.py
@@ -87,6 +87,7 @@ class TestLibrary(unittest.TestCase):
         results = self.library.search_books(title='Test-Driven')
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].title, 'Test-Driven')
+<<<<<<< HEAD
 
     def test_borrow_and_return_multiple_books(self):
         book1 = Book(isbn="1234567890", title="Test-Driven", author="dhruv", year=1925)
@@ -111,7 +112,29 @@ class TestLibrary(unittest.TestCase):
         isbns_available = [book.isbn for book in available]
         self.assertIn("1234567890", isbns_available)
         self.assertIn("1122334455", isbns_available)
+=======
+    def test_multiple_borrow_and_return(self):
+        
+        book1 = Book(isbn='1234567890', title='Book One', author='Author A', year=2021)
+        book2 = Book(isbn='0987654321', title='Book Two', author='Author B', year=2022)
+        book3 = Book(isbn='1122334455', title='Book Three', author='Author C', year=2023)
+        self.library.add_book(book1)
+        self.library.add_book(book2)
+        self.library.add_book(book3)
+        self.library.borrow_book(book1.isbn)
+        self.library.borrow_book(book2.isbn)
+        
+        self.assertTrue(book1.is_borrowed)
+        self.assertTrue(book2.is_borrowed)
+        self.assertFalse(book3.is_borrowed)  
+>>>>>>> ea23299 (Add test for multiple borrow and return)
 
+        self.library.return_book(book1.isbn)
+        self.assertFalse(book1.is_borrowed)
+        
+        self.assertTrue(book2.is_borrowed)
+        with self.assertRaises(ValueError):
+            self.library.return_book(book3.isbn)
 
 if __name__ == '__main__':
     unittest.main()
